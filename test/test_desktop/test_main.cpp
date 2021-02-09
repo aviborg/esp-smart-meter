d97df185f26ed7c5567aed53c06e0335d4ea274d
AndreasV <andreas.viborg@gmail.com>
2021-02-09 22:44:54 +0100

    added second ascii test case

--- test/test_desktop/test_main.cpp
+++ test/test_desktop/test_main.cpp
@@ -16,10 +16,11 @@ void testAXDR(void) {
     TEST_ASSERT_EQUAL(0, strcmp(resultStr, axdr1Str));
 }
 
-void testASCII(void) {
+void testASCII1(void) {
     StaticJsonDocument<8192> jsonData;
     DlmsReader dmr;
-    dmr.setJson(&jsonData); 
+    dmr.setJson(&jsonData);
+
     TEST_ASSERT_TRUE(dmr.ParseData(ascii1Data, 678));
     serializeJson(jsonData, resultStr, 4096);
     printf(resultStr);
@@ -27,10 +28,23 @@ void testASCII(void) {
     TEST_ASSERT_EQUAL(0, strcmp(resultStr, ascii1Str));
 }
 
+void testASCII2(void) {
+    StaticJsonDocument<8192> jsonData;
+    DlmsReader dmr;
+    dmr.setJson(&jsonData);
+
+    TEST_ASSERT_TRUE(dmr.ParseData(ascii2Data, 712));
+    serializeJson(jsonData, resultStr, 4096);
+    printf(resultStr);
+    printf("\n");
+    TEST_ASSERT_EQUAL(0, strcmp(resultStr, ascii2Str));
+}
+
 int main(int argc, char **argv) {
     UNITY_BEGIN();
     RUN_TEST(testAXDR);
-    RUN_TEST(testASCII);
+    RUN_TEST(testASCII1);
+    RUN_TEST(testASCII2);
     UNITY_END();
 
     return 0;
