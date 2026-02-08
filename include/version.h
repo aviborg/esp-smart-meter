e6f120c171dd6a7ccec75c96b65e86dc1d3efd71
aviborg <5530227+aviborg@users.noreply.github.com>
2026-02-08 11:12:07 +0000

    Update firmware for release v0.0.4-alpha4

--- include/version.h
+++ include/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 // Version information - automatically updated by CI/CD
-#define FIRMWARE_VERSION "dev"
+#define FIRMWARE_VERSION "v0.0.4-alpha4"
 #define BUILD_TIMESTAMP __DATE__ " " __TIME__
 
 #endif // VERSION_H
